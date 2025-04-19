ba89ca8eb7af754f085cf125dc6d021b1f53d6c6
root <root@DESKTOP-NAYA.>
2025-04-19 15:44:43 +0100

    updated errors in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 import io
 import base64
 
-app = Flask(_name_)
+app = Flask(__name__)
 CSV_FILE = 'wellness_data.csv'
 
 # Create CSV file if it doesn't exist
@@ -59,5 +59,5 @@ def summary():
         print("Error:", e)
         return "Internal server error"
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True)
