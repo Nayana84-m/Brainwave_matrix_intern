775048500091a6892dacaec29cac848fc351e7af
root <root@DESKTOP-NAYA.>
2025-04-26 15:19:21 +0100

    Added app.run with dynamic port for deployment

--- app.py
+++ app.py
@@ -54,3 +54,6 @@ def garden():
 
     return render_template('garden.html', reflections=reflections)
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
